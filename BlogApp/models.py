bca34df8a75435d540b52e69316abb1fa36a622f
Ludis-ET <leulsegedmelaku1020@gmail.com>
2024-01-13 21:27:37 +0300

    creating the post class

--- BlogApp/models.py
+++ BlogApp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=255,unique=True)
+    content = models.TextField()
+    category = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='Post/')
+    tags = models.ArrayField(models.CharField(max_length=50))
+
+    def __str__(self):
+        return self.title
