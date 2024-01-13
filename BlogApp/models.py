bf8f243f08d8ca18179a02fe0e7bddf72a662511
Ludis-ET <leulsegedmelaku1020@gmail.com>
2024-01-13 21:40:18 +0300

    creating the Comment model

--- BlogApp/models.py
+++ BlogApp/models.py
@@ -5,7 +5,6 @@ class Post(models.Model):
     content = models.TextField()
     category = models.CharField(max_length=255)
     image = models.ImageField(upload_to='Post/')
-    tags = models.ArrayField(models.CharField(max_length=50))
-
+    tags = models.CharField(max_length=255, blank=True, null=True)
     def __str__(self):
         return self.title
--- CommentApp/models.py
+++ CommentApp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from BlogApp.models import Post
 
-# Create your models here.
+class Comment(models.Model):
+    content = models.TextField()
+    author = models.CharField(max_length=255)
+    published_date = models.DateTimeField(auto_now=True)
+    post = models.ForeignKey(Post,on_delete=models.CASCADE)
