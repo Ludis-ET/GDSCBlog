1820520ee52fc06ea306fb9dcbb4fabde93f9644
Ludis-et <leulsegedmelaku1020@gmail.com>
2024-02-29 12:38:48 +0300

    Extend Post model with Title, Content, Category, Image, and Tags fields

--- BlogApp/models.py
+++ BlogApp/models.py
@@ -1,3 +1,15 @@
+# In the "BlogApp" app, extend the "Post" model with the following fields and constraints:
+# Title (CharField, unique=True)
+# Content (TextField)
+# Category (CharField)
+# Image (ImageField)
+# Tags (ArrayField(CharField))
+
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    content = models.TextField()
+    category = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='blog/')
+    tags = models.ArrayField(models.CharField(max_length=100), blank=True)
