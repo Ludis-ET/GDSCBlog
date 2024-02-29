a75474c73cca9a890623e654fcdaa3b27a667e4b
Ludis-et <leulsegedmelaku1020@gmail.com>
2024-02-29 12:34:56 +0300

    creating a django project

--- CommentApp/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from BlogApp.models import Post
-
-class Comment(models.Model):
-    content = models.TextField()
-    author = models.CharField(max_length=255)
-    published_date = models.DateTimeField(auto_now=True)
-    post = models.ForeignKey(Post,on_delete=models.CASCADE)
