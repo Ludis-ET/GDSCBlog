e50e1cf613a9d9121334d31b2b07c92e73111e0d
Ludis-et <leulsegedmelaku1020@gmail.com>
2024-02-29 13:13:43 +0300

    The commit adds the Comment model to the CommentApp Django app. The Comment model has the following fields: - Content (TextField) - Author (CharField) - Published Date (DateTimeField) - Post (ForeignKey to the "Post" model)

--- CommentApp/models.py
+++ CommentApp/models.py
@@ -1,3 +1,14 @@
-from django.db import models
+# In the "CommentApp" app, create a model named "Comment" with the following fields:
+# Content (TextField)
+# Author (CharField)
+# Published Date (DateTimeField)
+# Post (ForeignKey to the "Post" model)
 
-# Create your models here.
+from django.db import models
+from BlogApp.models import Post
+
+class Comment(models.Model):
+    content = models.TextField()
+    author  = models.CharField(max_length=200)
+    published_date = models.DateTimeField(auto_now_add=True)
+    post    = models.ForeignKey(Post, on_delete=models.CASCADE)
