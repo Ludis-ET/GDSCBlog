307d4e40f46d0bef4d65bcf97fbba85cbe8c318c
Ludis-et <leulsegedmelaku1020@gmail.com>
2024-02-29 13:10:11 +0300

    Added a new model called Tags and updated the Post model to use ManyToManyField to link to the Tags model.

--- BlogApp/models.py
+++ BlogApp/models.py
@@ -7,9 +7,19 @@
 
 from django.db import models
 
+
+class Tags(models.Model):
+    name = models.CharField(max_length=20)
+    def __str__(self):
+        return self.name
+    
+
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     content = models.TextField()
     category = models.CharField(max_length=50)
     image = models.ImageField(upload_to='blog/')
-    tags = models.ArrayField(models.CharField(max_length=100), blank=True)
+    tags = models.ManyToManyField(Tags)
+    # You guys mentioned that this tags model should be an ArrayField, but when I used it, it was saying it only works on a PostgreSQL database. So, I created another model called 'tags' and included it.
+    def __str__(self):
+        return self.title
